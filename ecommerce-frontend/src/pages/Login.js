11fe3f7961f692e1bded5f61383d5fd0327aa620
Trud-Hong <hws7146@naver.com>
2026-02-18 22:16:27 +0900

    fix:사용 안하는 import 제거

--- ecommerce-frontend/src/pages/Login.js
+++ ecommerce-frontend/src/pages/Login.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import axios from 'axios';
 import {Link, useNavigate} from "react-router-dom";
 import api from '../api/axios';
 import { useAuth } from '../context/AuthContext';
